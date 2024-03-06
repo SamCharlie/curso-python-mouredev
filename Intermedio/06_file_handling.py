ad04b7340cf899083df6e95ab5217f33986b820d
Carlos <samc.z.1605@gmail.com>
2024-03-06 09:58:44 -0600

    Guardado

--- Intermedio/06_file_handling.py
+++ Intermedio/06_file_handling.py
@@ -2,5 +2,23 @@
 
 # .txt file
 
-txt_file = open("Intermedio/my_file.txt", "w+")
-print(txt_file.read())
+import os
+
+txt_file = open("Intermedio/my_file.txt", "w+") #Leer y escribir
+txt_file.write("\nMi nombre es Carlos\nMi apellido es Salazar\n28 años\ny estoy eprendiendo Python")
+
+#print(txt_file.read())
+#print(txt_file.read(10))
+#print(txt_file.readline())
+#print(txt_file.readline())
+#print(txt_file.readlines())
+for line in txt_file.readlines():
+    print(line)
+
+txt_file.write("\nY tambien tengo hambre")
+
+print(txt_file.readline())
+
+txt_file.close()
+
+#os.remove("Intermedio/my_file.txt")
