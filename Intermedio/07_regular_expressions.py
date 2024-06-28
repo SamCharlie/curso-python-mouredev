489deca2f2485922a50887c0253a3b4dfe4c0f9b
Carlos Salazar <samc.z.1605@gmail.com>
2024-06-27 19:27:12 -0600

    Cambios ultimos

--- Intermedio/07_regular_expressions.py
+++ Intermedio/07_regular_expressions.py
@@ -2,6 +2,8 @@
 
 import re 
 
+#match
+
 my_string = "Esta es la leccion número 7: Expresiones regulares"
 my_other_string = "Esta no es la leccion número 6: Manejo de ficheros"
 
@@ -10,9 +12,35 @@ print(match)
 start, end = match.span()
 print(my_string[start:end])
 
-match = print(re.match("Esta es la leccion",my_other_string))
-print(match)
-start, end = match.span()
-print(my_string[start:end])
+# match = print(re.match("Esta es la leccion", my_other_string))
+# if match == None:
+#     print(match)
+#     start, end = match.span()
+#     print(my_string[start:end])
 
 print(re.match("Expresiones regulares",my_string))
+
+#search
+
+search = re.search("Esta es la leccion", my_string, re.I)
+print(search)
+start, end = search.span()
+print(my_string[start:end])
+
+#findall
+
+findall = re.findall("leccion", my_string, re.I)
+print(findall)
+
+#split
+
+print(re.split(":", my_string))
+
+#sub
+
+print(re.sub("Expresiones regulares", "RegEx", my_string))
+
+#Patterns 
+
+pattern = r"[lL]eccion"
+print(re.findall(pattern), my_string)
---
+++ Intermedio/08_python_package_manager.py
@@ -0,0 +1,14 @@
+
+
+import numpy 
+
+print(numpy.version.version)
+
+numpy_array = numpy.array([34,25,25,86,34,29,53])
+print(type(numpy_array))
+
+print(numpy_array * 2)
+
+import pandas
+
+import requests
